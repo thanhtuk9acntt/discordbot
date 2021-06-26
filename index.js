a92716cf5f48aa3d19e0fb98cdd03a53977d36c7
thanhtuk9acntt <thanhtuk9acntt@gmail.com>
2021-06-27 01:21:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-const bot_token = 'ODUzOTI5MzY0NDYyMzcwODI2.YMchsQ.9mGiAOyBWY9XjOO6XT7lY0yuXIo';
+const bot_token = 'ODUzOTI1NTczNDY5NDA1MjA1.YMceKQ.P3fx7Fs7vsFgfZxyQ4wjvqEDqwk';
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const disbut = require('discord-buttons');
+disbut(client);
+let telegramButton = new disbut.MessageButton()
+  .setStyle('url')
+  .setLabel('Telegram Channel') 
+  .setURL('https://t.me/lunayieldann');
+
+  let discordButton = new disbut.MessageButton()
+  .setStyle('url')
+  .setLabel('Discord Channel') 
+  .setURL('https://discord.gg/f6WZN6qcgd');
+
+  let homePageButton = new disbut.MessageButton()
+  .setStyle('url')
+  .setLabel('Home page') 
+  .setURL('https://lunayield.com/');
+
+  let twitterButton = new disbut.MessageButton()
+  .setStyle('url')
+  .setLabel('Twitter') 
+  .setURL('https://twitter.com/Luna_yield');
+
+  let row = new disbut.MessageActionRow().addComponent(homePageButton).addComponent(telegramButton).addComponent(twitterButton).addComponent(discordButton);
+  let setting = {
+	GuildChannelId : '855309934149632010',
+	GuildChannelWelcomeMessage : 'Welcome to Luna Yield Lobby!',
+	EnableAttachment: false,
+	ReplyAttachmentMessage: "Not allow send url or image into here!!",
+	EnableURL: false,
+	ReplyURLMessage: "Not allow send url or image into here!!"
+ };
 
 
 client.on('ready', () => {
@@ -16,14 +47,78 @@ client.on('interaction', async interaction => {
 
 client.login(bot_token);
 client.on('guildMemberAdd', member => {
-  
+  member.guild.channels.cache.get(setting.GuildChannelId).send(setting.GuildChannelWelcomeMessage,{ component: row }); 
 });
+
 client.on('message', async message => {
 
     // Voice only works in guilds, if the message does not come from a guild,
     // we ignore it
     if (!message.guild) return;
+    
+	if(message.content.startsWith('!')){
+    if(message.content === '!hi'){
+      message.reply('Hi');
+      return;
+    }
+		if(message.content.startsWith('!edit'))
+		if(CheckRoles(message, 'Admin')){
+			var str = message.content;
+			str = str.replace(/  +/g, ' ');
+			var arr = str.split(' ');
+			var updateContent = str.substring(str.indexOf(arr[2])).trim();
+			if(arr && arr.length > 2){
+				switch (arr[1]) {
+					case 'GuildChannelId':
+						setting.GuildChannelId = updateContent;
+					
+						break;
+					case 'GuildChannelWelcomeMessage':
+						setting.GuildChannelWelcomeMessage = updateContent;
+					break;
+					case 'EnableAttachment':
+						setting.EnableAttachment =  updateContent.toLowerCase() == 'true';
+					break;
+					case 'ReplyAttachmentMessage':
+						setting.ReplyAttachmentMessage = updateContent;
+					break;
+					case 'EnableURL':
+						setting.EnableURL = updateContent.toLowerCase() == 'true';
+						break;
+					case 'ReplyURLMessage':
+						setting.ReplyURLMessage = updateContent;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		if(message.content.startsWith('!setting'))
+		if(CheckRoles(message, 'Admin'))
+		{
+			message.channel.send(JSON.stringify(setting));
+		}
 
+    if(message.content.startsWith('!channelId'))
+		if(CheckRoles(message, 'Admin'))
+		{
+			message.channel.send(message.channel.id);
+		}
+	}
+	if(CheckRoles(message, 'Admin')){
+		return;
+	}
+      if(message.author.bot) return;
+	if(CheckURL(message.content) && !setting.EnableURL){
+		message.delete();
+		message.channel.send(setting.ReplyURLMessage);
+		return;
+	}
+	  if(message.attachments && message.attachments.size > 0  && !setting.EnableAttachment ){
+		  message.delete();
+		  message.channel.send(setting.ReplyAttachmentMessage);
+		  return;
+	  }
   //  message.channel.send('Hi',disbut);	
    // client.channel.send('hi hi',disbut);
     if (message.content === '/join') {
@@ -34,9 +129,7 @@ client.on('message', async message => {
         message.reply('You need to join a voice channel first!');
       }
     }
-	if(message.content === '!hi'){
-		message.reply('Hi');
-	}
+  
   });
 
 

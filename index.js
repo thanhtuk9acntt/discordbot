a00bc1612b928a7979b8ee47bcfd01b97ce88f8b
thanhtuk9acntt <thanhtuk9acntt@gmail.com>
2021-06-29 10:00:54 +0700

    Update index.js
    update token

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const bot_token = 'ODUzOTI1NTczNDY5NDA1MjA1.YMceKQ.P3fx7Fs7vsFgfZxyQ4wjvqEDqwk';
+const bot_token = 'ODUzOTI1NTczNDY5NDA1MjA1.YMceKQ.BakQF6DqC4tH4cgcbUwacPw8Q4o';
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const disbut = require('discord-buttons');
